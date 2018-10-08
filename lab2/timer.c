9678076dd2cbc7ac70e86ed7e7d6c0ea229456c6
up201703782 <up201703782@77310814-a185-4aa2-afa5-df2ae37f3f86>
2018-10-08 21:55:17 +0000

    Correcoes timer_display_conf e timer_get_conf

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,7 +6,7 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t UNUSED(freq)) {
-
+  
   uint8_t st, lsb, msb;
   timer_get_conf(timer, &st);
   util_get_LSB(st, &lsb);
@@ -40,17 +40,43 @@ void (timer_int_handler)() {
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   
   uint8_t RB_command = TIMER_RB_CMD | TIMER_RB_SEL(timer);
+
+  timer = TIMER_0 + timer;
+
   int check = sys_outb(TIMER_CTRL,RB_command);
-  sys_outb(TIMER_CTRL,RB_command);
 
   if(check == 1){
     perror("Error");
     return 1;
   }
+
   uint32_t adress = (uint32_t) *st;
+  int check1=0;
+  switch(timer)
+ {case 0:
+    {
+     check1 = sys_inb(TIMER_0, &adress);
+     break;
+    }
+case 1:
+    {
+     check1 = sys_inb(TIMER_1, &adress);
+     break;
+    }
+case 2:
+    {
+     check1 = sys_inb(TIMER_2, &adress);
+     break;
+    }
+ }
 
-  check = sys_inb(timer + TIMER_0, &adress);
+  if(check1 == 1)
+  {
+    perror("Error");
+    return 1;
+  }
 
+  //*st = (uint8_t)newst;
   return 0;
 }
 
@@ -58,7 +84,36 @@ int (timer_display_conf)(uint8_t timer, uint8_t st,
                        enum timer_status_field field) {
 
   union timer_status_field_val conf;
-  conf.byte = st;
+
+
+
+  switch(field)
+  {
+
+    case all:
+    {
+      conf.byte= st;
+      break;
+    }
+    case initial:
+    {
+    //10100010  TIMER_LSB_MSB  00110000 00100000 00000010
+      conf.in_mode = (st & TIMER_LSB_MSB) >> 4;
+      break;
+    }
+
+    case mode:
+    {
+      conf.count_mode = (st & TIMER_SQR_WAVE) >> 1;
+      break;
+    }
+
+    case base:
+    {
+      conf.bcd = (st & TIMER_BCD);
+    }
+
+  }
 
   timer_print_config(timer, field, conf);
 
