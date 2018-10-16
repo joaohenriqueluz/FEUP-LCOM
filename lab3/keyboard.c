608870ee6745a4dd93274d235b3e717d365e61d2
Francicsco Rodrigues <44161051+franciscoLuzRodrigues@users.noreply.github.com>
2018-10-16 11:55:56 +0000

    Funcao kb_scan_byte() completa. Funcao kbd_test_scan() incompleta.

--- lab3/keyboard.c
+++ lab3/keyboard.c
@@ -5,12 +5,14 @@
 
 #include "i8042.h"
 
+extern int byteCount;
+
 int globalHookId;
 
 int (kb_subscribe)(uint8_t *bit_no){
 	int temp_hook = TEMP_HOOK;
   
-  	sys_irqsetpolicy(KB_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &temp_hook);
+  	sys_irqsetpolicy(KB_IRQ, (IRQ_REENABLE | IRQ_EXCLUSIVE), &temp_hook);
 
   	globalHookId = temp_hook;
 
@@ -25,3 +27,25 @@ int (kb_unsubscribe)(){
 
   return 0;
 }
+
+uint8_t (kb_scan_byte)(){
+	uint32_t stat, data;
+	while( 1 ){
+	 sys_inb(KB_STATUS_REG, &stat); /* assuming it returns OK */
+        /* loop while 8042 output buffer is empty */
+        if( stat & OBF ) {
+            sys_inb(OUT_BUF, &data); /* assuming it returns OK */
+            if ( (stat &(PAR_ERR | TO_ERR)) == 0 )
+                return data;
+            else
+                return -1;
+        }
+        tickdelay(micros_to_ticks(DELAY_US));
+}
+
+
+
+
+
+
+
--- lab3/lab3.c
+++ lab3/lab3.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+int byteCount = 0;
+
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
   lcf_set_language("EN-US");
@@ -28,8 +30,58 @@ int main(int argc, char *argv[]) {
 }
 
 int (kbd_test_scan)(bool UNUSED(assembly)) {
-	/* To be completed */
-	/* When you use argument assembly for the first time, delete the UNUSED macro */
+
+  int ipc_status;
+  message msg;
+  unsigned int r;
+  uint8_t bit_no;
+  uint8_t size;
+  bool is_over = false;
+  uint8_t* byte;
+  uint8_t fst_byte;
+  uint8_t sd_byte;
+  bool make = true;
+
+  kb_subscribe(&bit_no);
+
+ uint32_t irq_set = BIT(bit_no);
+
+  while(!is_over) {
+
+     if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+          printf("driver_receive failed with: %d", r);
+         continue;
+     } 
+     if (is_ipc_notify(ipc_status)) { /* received notification */
+         switch (_ENDPOINT_P(msg.m_source)) {
+             case HARDWARE: /* hardware interrupt notification */       
+                 if (msg.m_notify.interrupts & irq_set) {
+                    byte = kb_scan(size);
+                    if (byte[1] == ESC_BREAK){
+                      is_over= true;
+                      continue;
+                    }
+                    if (byte[1] > 0x7f)
+                    {
+                      make = false
+                    }
+
+                    kbd_print_scancode(make, size, byte);
+
+                  }
+                 break;
+              default:
+                  break; /* no other notifications expected: do nothing */ 
+         }
+     } else { /* received a standard message, not a notification */
+         /* no standard messages expected: do nothing */
+     }
+     //delay(WAIT_KBC);
+ }
+
+  kb_unsubscribe();
+
+  return 0;
 }
 int (kbd_test_poll)() {
     /* To be completed */
