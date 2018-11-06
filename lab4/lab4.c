aeb6bb613502fda5fd34d496f0d4e9ff442cb9fe
up201703782 <up201703782@77310814-a185-4aa2-afa5-df2ae37f3f86>
2018-11-06 19:03:44 +0000

    mouse_test_packet a funcionar sem warningspwd

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -108,9 +108,51 @@ disable_mouse();
 }
 
 int (mouse_test_remote)(uint16_t period, uint8_t cnt) {
-    /* To be completed */
-    printf("%s(%u, %u): under construction\n", __func__, period, cnt);
-    return 1;
+ 
+  struct packet *pp = malloc(sizeof(struct packet));
+  uint8_t count = 0, packet[3];
+
+
+if(write_comand_mouse() != 0)
+    {
+      printf("Problema ao enviar o 1º comando\n");
+    }
+
+
+disable_mouse();     
+
+if(mouse_enable_remote() != 0)
+    {
+      printf("Problema ao enviar o comando ENABLE REMOTE MODE\n");
+    }
+
+while(count < cnt)
+  {
+    mouse_remote(period);
+    packet[byteCounter++]= byte;
+    if (byteCounter >= 2)
+    {
+      for (int i = 0; i < 3; i++)
+      {
+        pp->bytes[i] = packet[i];
+      }
+      pp->rb = (pp->bytes[0] & BIT(1) >> 1);
+      pp->mb = (pp->bytes[0] & BIT(2) >> 2);
+      pp->lb = (pp->bytes[0] & BIT(0));
+      pp->x_ov = (pp->bytes[0] & BIT(6) >> 6);
+      pp->y_ov = (pp->bytes[0] & BIT(7) >> 7);
+      pp->delta_x = pp->bytes[1] | (pp->bytes[0] & BIT(4) << 12);
+      pp->delta_y = pp->bytes[2] | (pp->bytes[0] & BIT(5) << 11);
+
+      mouse_print_packet(pp);
+      byteCounter = 0;
+      count++;
+    }
+  }
+uint8_t default_cmd= minix_get_dflt_kbc_cmd_byte();
+  sys_outb(KB_STATUS_REG,WRITE_TO_MOUSE);
+  sys_outb(KB_STATUS_REG, default_cmd);
+return 0;
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
