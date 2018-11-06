0d01d51ca8e6aaa8b13fc17293de7ed07aa8b399
Francicsco Rodrigues <44161051+franciscoLuzRodrigues@users.noreply.github.com>
2018-11-06 11:51:02 +0000

    Função mouse_test_packet já funciona mas falta fazer o reset no fim

--- lab4/i8042.h
+++ lab4/i8042.h
@@ -36,6 +36,7 @@ int sys_inb_cnt(port_t port, uint32_t *byte);
 
 #define WRITE_TO_MOUSE     0xd4
 #define STREAM_MODE        0xea
+#define MOUSE_RESET        0xff
 #define ACK     0xfa
 #define NACK    0xfe
 #define ERROR   0xfc
