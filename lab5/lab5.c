48ee20d33099e7736faad746576981d11b5da3d7
up201808909 <up201808909@77310814-a185-4aa2-afa5-df2ae37f3f86>
2018-11-17 23:02:01 +0000

    test_rectangle a funcionar

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
 
 int (video_test_init)(uint16_t mode, uint8_t delay) {
   if(vg_init(mode) == NULL){
-    printf("Erro na função vg_enter\n");
+    printf("Erro na função vg_init\n");
     return 1;
   }
 
@@ -60,12 +60,12 @@ int (video_test_init)(uint16_t mode, uint8_t delay) {
 int (video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
                        uint16_t width, uint16_t height, uint32_t color) {
 
+  mode_global = mode;
+
   if (vg_init(mode) == NULL){
     printf("Erro na função vg_enter\n");
     return 1;
   }
-
-  mode_global = mode;
 
   if (vg_draw_rect(x,y,width,height,color) != 0)
   {
@@ -87,9 +87,9 @@ int (video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
   return 0;
 }
 
-int (video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first) {
-  /* To be completed */
-  printf("%s(0x%03x, %u, 0x%08x): under construction\n", __func__, mode, no_rectangles, first);
+// int (video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first) {
+//   /* To be completed */
+//   printf("%s(0x%03x, %u, 0x%08x): under construction\n", __func__, mode, no_rectangles, first);
 
-  return 1;
-}
+//   return 1;
+// }
