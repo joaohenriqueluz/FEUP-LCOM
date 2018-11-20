28b2fe17dd8a297cb9043cbf47b43a5c92adf974
Francicsco Rodrigues <44161051+franciscoLuzRodrigues@users.noreply.github.com>
2018-11-20 11:18:36 +0000

    Inicio video_test_pattern

--- lab5/mode.h
+++ lab5/mode.h
@@ -11,8 +11,12 @@ int map_vram(vbe_mode_info_t *vmi_p);
 
 //int num_bytes_mode(uint16_t mode);
 
-int vg_draw_rect(uint16_t x, uint16_t y, uint16_t len, uint16_t height, uint32_t color);
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t len, uint16_t height, uint32_t color);
 
 int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+
+int draw_matrix(uint8_t no_rectangles, uint32_t first, uint8_t step);
+
+int draw_indexed(uint8_t no_rectangles, uint32_t first, uint8_t step);
 
 #endif
