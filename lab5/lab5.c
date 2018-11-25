471c59d2bfc3cad1c03867544e5dd22ddd1fae09
Francicsco Rodrigues <44161051+franciscoLuzRodrigues@users.noreply.github.com>
2018-11-25 18:56:02 +0000

    correcao na nossa get_info

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -171,46 +171,10 @@ int (video_test_move)(const char *xpm[], uint16_t xi, uint16_t yi, uint16_t xf, 
 }
 
 int (video_test_controller)() {
-  struct reg86u r;
-  mmap_t m;
-
-  lm_init(true);
-
-  vg_vbe_contr_info_t *info_p = malloc(sizeof(vg_vbe_contr_info_t));
-  lm_alloc(sizeof(mmap_t), &m);                /* use liblm.a to initialize buf */
-  phys_bytes buf = m.phys;
-
-  memset(&r, 0, sizeof(r)); /* zero the structure */
-
-  info_p->VBESignature[0] = 'V';
-  info_p->VBESignature[1] = 'B';
-  info_p->VBESignature[2] = 'E';
-  info_p->VBESignature[3] = '2';
-
-  r.u.w.ax = VBE_INFO;          /* VBE get mode info */
-  /* translate the buffer linear address to a far pointer */
-  r.u.w.es = PB2BASE(buf);    /* set a segment base */
-  r.u.w.di = PB2OFF(buf);      /*set the offset accordingly */
-  r.u.w.cx = mode;
-  r.u.b.intno = 0x10;
-
-  if( sys_int86(&r) != OK ){
-    printf("vg_get_info(): sys_int86() failed \n");
-    return 1;
-  }
-
-  info_p = (vg_vbe_contr_info_t *) m.virt;
-
-  lm_free(&m);
-
-  if (vg_display_vbe_contr_info(info_p) != 0)
+  if (get_controller() != 0)
   {
-    printf("Erro a imprimeir na função video_test_controller\n");
+    printf("Erro na função get_controller\n");
     return 1;
   }
-
-  lm_free(&m);
-
-
   return 0;
 } 
