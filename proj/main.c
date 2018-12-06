83409c2adc1df5f281f87c48fa4a9d70b6e5e314
up201703782 <up201703782@77310814-a185-4aa2-afa5-df2ae37f3f86>
2018-12-06 17:29:36 +0000

    Moves Player

--- proj/main.c
+++ proj/main.c
@@ -39,9 +39,85 @@ int main(int argc, char *argv[]) {
 
 int (proj_main_loop)() {
   vg_init(0x144);
-  Bitmap* new_bmp = loadBitmap("/home/lcom/labs/proj/imagens/spacecomdiogo.bmp");
-  drawBitmap(new_bmp,0,0,ALIGN_LEFT);
-  sleep(25);
+  Bitmap* background = loadBitmap("/home/lcom/labs/proj/imagens/spacecomdiogo.bmp");
+  Bitmap* ship = loadBitmap("/home/lcom/labs/proj/imagens/blue1.bmp");
+
+int ipc_status, ind = 0;
+  message msg;
+  unsigned int r;
+  uint8_t bit_no_timer, bit_no_kb, scancode1[1], scancode2[2];
+  //uint8_t seconds = 0;
+
+  if(kb_subscribe(&bit_no_kb) != 0)
+    printf("Erro na funcao kb_subscribe\n");
+
+  if(timer_subscribe_int(&bit_no_timer) != 0)
+    printf("Erro na funcao timer_subscribe_int\n");
+
+  uint32_t irq_set_kb = BIT(bit_no_kb);
+  uint32_t irq_set_timer = BIT(bit_no_timer);
+
+  drawBitmap(background,0,0,ALIGN_LEFT);
+
+  drawBitmap(ship, 487, 690, ALIGN_LEFT);
+
+  while(!is_over) {
+
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 )
+      { 
+        printf("driver_receive failed with: %d", r);
+        continue;
+      } 
+    if (is_ipc_notify(ipc_status))
+      { /* received notification */
+        switch (_ENDPOINT_P(msg.m_source))
+        {
+          case HARDWARE: /* hardware interrupt notification */      
+
+          if (msg.m_notify.interrupts & irq_set_timer)
+            {
+              timer_int_handler();
+              //printf("sec = %d\n", seconds);
+            }
+
+            if (msg.m_notify.interrupts & irq_set_kb)
+              {
+                  globalCounter = 0;
+
+                  kbd_read();
+                  move_ship(ship,  background);
+                  if (size == 2)
+                  {
+                    scancode2[ind] = byte;
+                  }
+                  else{
+                    scancode1[ind] = byte;
+                  }
+                  ind++;
+              }
+            
+         }
+     } else { /* received a standard message, not a notification */
+         /* no standard messages expected: do nothing */}
+
+     if (ind >= size)
+      {
+        make = true;
+        size = 1;
+        ind = 0;
+      }
+
+ 
+  
+    //drawBitmap(ship,100,100,ALIGN_LEFT);
+
+      
+ }
+  if(timer_unsubscribe_int() != 0)
+    printf("Erro na funcao timer_unsubscribe_int\n");
+  if(kb_unsubscribe() != 0)
+    printf("Erro na funcao kb_unsubscribe\n");
+
   vg_exit();
 
   return 0;
