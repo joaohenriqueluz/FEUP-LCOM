c6bd87f7088a3d824d034ea6f2309ad530cdc28b
Francicsco Rodrigues <44161051+franciscoLuzRodrigues@users.noreply.github.com>
2018-12-11 12:34:19 +0000

    Colision check por coordenadas

--- proj/game.c
+++ proj/game.c
@@ -7,6 +7,7 @@
 
 int shotX, shotY;
 extern int h_res, v_res;
+bool alive = true;
 
 Jogo* inicio(){
 	Jogo* jogo = (Jogo*) malloc(sizeof(Jogo));
@@ -19,6 +20,12 @@ Jogo* inicio(){
 
 	jogo->shot_map =  redLaser_xpm;
 	jogo->shot_pic = xpm_load(jogo->shot_map, XPM_5_6_5, &jogo->shot_info);
+
+	jogo->alien_map =  alien_xpm;
+	jogo->alien_pic = xpm_load(jogo->alien_map, XPM_5_6_5, &jogo->alien_info);
+
+	jogo->bang_map =  explosion_xpm;
+	jogo->bang_pic = xpm_load(jogo->bang_map, XPM_5_6_5, &jogo->bang_info);
 
 	return jogo;
 }
@@ -73,10 +80,10 @@ void playerDelete(Player* player){
 	free(player);
 }
 
-Alien* alienInit(){
+Alien* alienInit(Jogo* jogo){
 	Alien* frank = (Alien*) malloc(sizeof(Alien));
 
-	frank->x = h_res/2;
+	frank->x = h_res/2 - (jogo->alien_info.width)/2;
 	frank->y = 0;
 	frank->speed = 0;
 
@@ -87,18 +94,38 @@ void alienDelete(Alien* frank){
 	free(frank);
 }
 
-void drawJogo(Jogo* mib, Player* willsmith){
+void drawJogo(Jogo* mib, Player* willsmith, Alien* frank){
 	vg_draw_xpm(mib->background_pic, &mib->background_info, 0, 0);
 	vg_draw_xpm(mib->ship_pic, &mib->ship_info, willsmith->x, willsmith->y);
 	if (willsmith->shot)
 	{
+		int minY = frank->y;
+		int minX = frank->x;
+		int maxY = frank->y + (mib->alien_info.height);
+		int maxX = frank->x + (mib->alien_info.width);
+		if ((shotY >= minY && shotY <= maxY && shotX >= minX && shotX <= maxX))
+		{
+			vg_draw_xpm(mib->bang_pic, &mib->bang_info, frank->x, frank->y);
+			sleep(2);
+			alive = false;
+		}
 		if (shotY - 10 >= 0)
 		{
 			shotY -= 10;
 			vg_draw_xpm(mib->shot_pic, &mib->shot_info, shotX, shotY);
+			if (alive)
+			{
+				vg_draw_xpm(mib->alien_pic, &mib->alien_info, frank->x, frank->y);
+			}
 			return;
 		}
 	}
+	
+		if (alive)
+		{
+			vg_draw_xpm(mib->alien_pic, &mib->alien_info, frank->x, frank->y);
+		}
+	
 	willsmith->shot = 0;
 }
 
