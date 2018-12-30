042f8818872d71428badcce3ebdf2e7a63c04131
João Henrique Luz <44161011+joaohenriqueluz@users.noreply.github.com>
2018-12-30 01:29:14 +0000

    Adicionados ficheiros em falta

---
+++ proj/RTC.c
@@ -0,0 +1,73 @@
+#include <lcom/lcf.h>
+#include <machine/int86.h> // /usr/src/include/arch/i386
+#include "RTC.h"
+
+void wait_valid_rtc() 
+{
+	uint32_t regA = 0;
+	do 
+	{
+		
+		sys_outb(RTC_ADDR_REG, RTC_REG_A);
+		sys_inb(RTC_DATA_REG, &regA);
+		
+	} while ( regA & RTC_UIP);
+}
+
+
+uint32_t read_reg(int reg)
+{
+	uint32_t value = 0;
+
+	//wait_valid_rtc();
+
+	if(sys_outb(RTC_ADDR_REG, reg) != OK)
+	{
+		printf("Error  in fucntion sys_outb(RTC_ADDR,reg)\n");
+		return 1;
+	}
+	if(sys_inb(RTC_DATA_REG,&value) != OK)
+	{
+		printf("Error  in fucntion sys_inb(RTC_DATA,value)\n");
+		return 1;
+	}
+
+	if((RTC_REG_B & BCD_MODE) == 0)
+		value = ((value & 0xF0) >> 4) *10 + (value & 0x0F);
+	
+	return value;
+}
+
+
+uint32_t get_Seconds()
+{
+	return read_reg(RTC_SECONDS);
+}
+
+uint32_t get_Minute()
+{
+	return read_reg(RTC_MINUTES);
+}
+
+uint32_t get_Hour()
+{
+	return read_reg(RTC_HOURS);
+}
+
+uint32_t get_Day()
+{
+	return read_reg(RTC_DAYS);
+}
+
+
+uint32_t get_Month()
+{
+	return read_reg(RTC_MONTH);
+}
+
+uint32_t get_Year()
+{
+	return read_reg(RTC_YEAR);
+}
+
+
---
+++ proj/RTC.h
@@ -0,0 +1,32 @@
+#ifndef _RTC_H_
+#define _RTC_H_
+
+#define RTC_ADDR_REG	0X70
+#define RTC_DATA_REG	0X71
+#define RTC_REG_A		0x0A
+#define RTC_REG_B		0x0B
+#define RTC_REG_C		12
+#define RTC_REG_D		13
+
+#define RTC_SECONDS  	0x00
+#define RTC_MINUTES  	0x02
+#define RTC_HOURS	  	0x04
+#define RTC_DAYS	  	0x07
+#define RTC_MONTH	  	0x08
+#define RTC_YEAR  		0x09
+
+#define RTC_UIP			(0x01 << 7)
+#define BCD_MODE		(0x01 << 2)
+
+
+
+void wait_valid_rtc();
+uint32_t read_reg(int reg);
+uint32_t get_Seconds();
+uint32_t get_Minute();
+uint32_t get_Hour();
+uint32_t get_Day();
+uint32_t get_Month();
+uint32_t get_Year();
+
+#endif
